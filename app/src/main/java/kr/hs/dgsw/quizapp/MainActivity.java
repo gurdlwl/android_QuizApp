1ac8f13cb94ea70346d83b9cab18041db02cf602
JunHyuk Im <31408848+gurdlwl@users.noreply.github.com>
2019-04-23 11:31:00 +0900

    [ADD]

---
+++ app/src/main/java/kr/hs/dgsw/quizapp/MainActivity.java
@@ -0,0 +1,22 @@
+package kr.hs.dgsw.quizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onQuizActivity(View view) {
+
+    }
+
+    public void onQuestionListActivity(View view) {
+
+    }
+}
