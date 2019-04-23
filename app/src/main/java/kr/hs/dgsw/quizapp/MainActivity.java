b5098a9793e47f5fc52af49c77ea201bfd475a3c
JunHyuk Im <31408848+gurdlwl@users.noreply.github.com>
2019-04-23 12:24:33 +0900

    [UPDATE]

--- app/src/main/java/kr/hs/dgsw/quizapp/MainActivity.java
+++ app/src/main/java/kr/hs/dgsw/quizapp/MainActivity.java
@@ -1,22 +1,33 @@
 package kr.hs.dgsw.quizapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private RadioGroup rg = findViewById(R.id.group);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //rg.setOnCheckedChangeListener();
     }
 
-    public void onQuizActivity(View view) {
 
+
+    public void onQuizActivity(View view) {
+        startActivity(new Intent(this, QuizActivity.class));
     }
 
     public void onQuestionListActivity(View view) {
-
+        startActivity(new Intent( this, QuestionListActivity.class));
     }
+
+
 }
