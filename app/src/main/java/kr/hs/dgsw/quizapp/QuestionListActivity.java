af60c66eff2251ed6f0aa894b6240e0e8c2bbfa9
gurdlwl <gurdlwl@gmail.com>
2019-05-11 14:43:27 +0900

    [UPDATE]

--- app/src/main/java/kr/hs/dgsw/quizapp/QuestionListActivity.java
+++ app/src/main/java/kr/hs/dgsw/quizapp/QuestionListActivity.java
@@ -3,17 +3,33 @@ package kr.hs.dgsw.quizapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Toast;
 
-public class QuestionListActivity extends AppCompatActivity {
+public class QuestionListActivity extends AppCompatActivity implements ListClickListener{
+
+    RecyclerView list;
+    LinearLayoutManager layoutManager;
+    ListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_list);
+
+        list = findViewById(R.id.questionList);
+        layoutManager = new LinearLayoutManager(this);
+        adapter = new ListAdapter(this);
     }
 
     public void onSetting(View view){
         startActivity(new Intent(this, SettingActivity.class));
+    }
+
+    @Override
+    public void onClick(View v, int position) {
+        Toast.makeText(this, "click!", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/kr/hs/dgsw/quizapp/SettingActivity.java
+++ app/src/main/java/kr/hs/dgsw/quizapp/SettingActivity.java
@@ -1,5 +1,6 @@
 package kr.hs.dgsw.quizapp;
 
+import android.content.Intent;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +16,11 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class SettingActivity extends AppCompatActivity {
+
+    public final int REQUEST_CODE1 =  1;
+    public final int REQUEST_CODE2 =  2;
+    public final int REQUEST_CODE3 =  3;
+    public final int REQUEST_CODE4 =  4;
 
     LinearLayout textLayout;
     LinearLayout imageLayout;
@@ -33,6 +39,22 @@ public class SettingActivity extends AppCompatActivity {
     ImageView iv2;
     ImageView iv3;
     ImageView iv4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        setting();
+        setLayout("text"); //텍스트로 초기화
+
+        tb.setOnClickListener(v -> {
+            if(tb.isChecked()) //사진
+                setLayout("image");
+            else if(!tb.isChecked()) //텍스트
+                setLayout("text");
+        });
+    }
 
     private void setting(){
         textLayout = (LinearLayout) findViewById(R.id.layoutText);
@@ -56,25 +78,6 @@ public class SettingActivity extends AppCompatActivity {
         iv4 = (ImageView) findViewById(R.id.imageView4);
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
-
-        setting();
-        setLayout("text"); //텍스트로 초기화
-
-        tb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(tb.isChecked()) //사진
-                    setLayout("image");
-                else if(!tb.isChecked()) //텍스트
-                    setLayout("text");
-            }
-        });
-    }
-
     private void setLayout(String type){
         if(type == "text"){
             textLayout.setVisibility(View.VISIBLE);
@@ -86,18 +89,47 @@ public class SettingActivity extends AppCompatActivity {
     }
 
     public void onImageClick(View v){
-        String id = String.format(String.valueOf(v.getId()));
-        Toast.makeText(this, "this message called by " + id + ", " + iv1.getId(), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
 
         //초기화된 이미지의 ID와 받아온 ID를 확인해서 데이터 삽입
-        if(id == String.format(String.valueOf(iv1.getId()))){
-
-        } else if (id == String.format(String.valueOf(iv2.getId()))){
-
-        } else if (id == String.format(String.valueOf(iv3.getId()))){
-
-        } else if (id == String.format(String.valueOf(iv4.getId()))){
-
+        switch(v.getId()){
+            case R.id.imageView1:
+                startActivityForResult(intent, REQUEST_CODE1);
+                break;
+            case R.id.imageView2:
+                startActivityForResult(intent, REQUEST_CODE2);
+                break;
+            case R.id.imageView3:
+                startActivityForResult(intent, REQUEST_CODE3);
+                break;
+            case R.id.imageView4:
+                startActivityForResult(intent, REQUEST_CODE4);
+                break;
         }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode == RESULT_OK){
+            if(requestCode == REQUEST_CODE1)
+                iv1.setImageURI(data.getData());
+            else if(requestCode == REQUEST_CODE2)
+                iv2.setImageURI(data.getData());
+            else if(requestCode == REQUEST_CODE3)
+                iv3.setImageURI(data.getData());
+            else if(requestCode == REQUEST_CODE4)
+                iv4.setImageURI(data.getData());
+        }
+    }
+
+    // radiobox 체크되어있는 문항, 문제 내용 DB에 저장
+    public void onDelete(View view){
+
+    }
+
+    public void onSave(View view){
+
     }
 }
